a9b12c2a7f6e70a6176b533b3663ee39adcd60e4
ShriRamaMoorthy <115894164+ShriRamaMoorthy@users.noreply.github.com>
2026-02-25 23:26:27 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-try:
-    import streamlit as st
-    OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", os.getenv("OPENAI_API_KEY", ""))
-except Exception:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
